b965b450b2680844435df6e2aadec27fee70f255
cfdaica123 <soncaodarkin3@gmail.com>
2023-09-29 08:23:43 +0700

    commit8:23

--- FoodApp/app/src/main/java/son/dev/foodapp/HomeActivity.java
+++ FoodApp/app/src/main/java/son/dev/foodapp/HomeActivity.java
@@ -1,21 +1,78 @@
 package son.dev.foodapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationBarView;
 
 import java.util.List;
 
 import son.dev.foodapp.constract.HomeConstract;
 import son.dev.foodapp.data.model.Product;
+import son.dev.foodapp.fragment.FavouriteFragment;
+import son.dev.foodapp.fragment.HomeFragment;
+import son.dev.foodapp.fragment.LibraryFragment;
+import son.dev.foodapp.fragment.SettingFragment;
 
 public class HomeActivity extends AppCompatActivity implements HomeConstract.IView {
-
+    private BottomNavigationView bottomNavigationView;
+    private FloatingActionButton fab;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setBackground(null);
+        bottomNavigationView.getMenu().getItem(2).setEnabled(true);
+
+        Fragment homeFragment = new HomeFragment();
+        Fragment favouriteFragment = new FavouriteFragment();
+        Fragment libraryFragment = new LibraryFragment();
+        Fragment settingFragment = new SettingFragment();
+
+        setCurrentFragment(homeFragment);
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if(item.getItemId() == R.id.menu_home){
+                    setCurrentFragment(homeFragment);
+                }
+                if(item.getItemId() == R.id.menu_favourite){
+                    setCurrentFragment(favouriteFragment);
+                }
+                if(item.getItemId() == R.id.menu_library){
+                    setCurrentFragment(libraryFragment);
+                }
+                if(item.getItemId() == R.id.menu_setting){
+                    setCurrentFragment(settingFragment);
+                }
+                return false;
+            }
+        });
+
+        fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HomeActivity.this, DetailActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void setCurrentFragment(Fragment fragment) {
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, fragment).commit();
     }
 
     private void init(){
---
+++ FoodApp/app/src/main/java/son/dev/foodapp/fragment/SettingFragment.java
@@ -0,0 +1,21 @@
+package son.dev.foodapp.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import son.dev.foodapp.R;
+
+public class SettingFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_setting, container, false);
+        return rootView;
+    }
+}
